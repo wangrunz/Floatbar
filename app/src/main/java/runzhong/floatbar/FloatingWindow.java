bc81b0d70a74bea62b10be6b399c3e0694ebc32d
wrz1994 <wrz1994@gmail.com>
2016-11-03 20:45:47 -0400

    Floating window example

--- app/src/main/java/runzhong/floatbar/FloatingWindow.java
+++ app/src/main/java/runzhong/floatbar/FloatingWindow.java
@@ -7,16 +7,19 @@ import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
-/**
- * Created by George on 11/3/2016.
- */
+
 public class FloatingWindow extends Service{
 
     private WindowManager wm;
     private LinearLayout ll;
+    private Button stop;
 
     @Nullable
     @Override
@@ -29,16 +32,54 @@ public class FloatingWindow extends Service{
 
         wm =(WindowManager)getSystemService(WINDOW_SERVICE);
         ll = new LinearLayout(this);
-
+        stop = new Button(this);
+        ViewGroup.LayoutParams btnParameter = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        stop.setText("stop");
+        stop.setLayoutParams(btnParameter);
         LinearLayout.LayoutParams llParameters = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
         ll.setBackgroundColor(Color.argb(66,255,0,0));
         ll.setLayoutParams(llParameters);
 
-        WindowManager.LayoutParams parameters = new WindowManager.LayoutParams(400,150,WindowManager.LayoutParams.TYPE_PHONE,WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSPARENT);
+        final WindowManager.LayoutParams parameters = new WindowManager.LayoutParams(400,150,WindowManager.LayoutParams.TYPE_PHONE,WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSPARENT);
         parameters.x=0;
         parameters.y=0;
-        parameters.gravity= Gravity.CENTER|Gravity.CENTER;
+        parameters.gravity= Gravity.CENTER;
 
+        ll.addView(stop);
         wm.addView(ll,parameters);
+        ll.setOnTouchListener(new View.OnTouchListener(){
+            private WindowManager.LayoutParams updatedParameters = parameters;
+            int x,y;
+            float touchedX, touchedY;
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent event){
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        x = updatedParameters.x;
+                        y = updatedParameters.y;
+
+                        touchedX=event.getRawX();
+                        touchedY=event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        updatedParameters.x=(int)(x+(event.getRawX()-touchedX));
+                        updatedParameters.y=(int)(y+(event.getRawY()-touchedY));
+
+                        wm.updateViewLayout(ll,updatedParameters);
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                wm.removeView(ll);
+                stopSelf();
+            }
+        });
     }
 }
--- app/src/main/java/runzhong/floatbar/MainActivity.java
+++ app/src/main/java/runzhong/floatbar/MainActivity.java
@@ -1,15 +1,18 @@
 package runzhong.floatbar;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
-
+    public final static int REQUEST_CODE= 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +20,28 @@ public class MainActivity extends AppCompatActivity {
 
         button=(Button)findViewById(R.id.button);
 
+
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                startService(new Intent(MainActivity.this,FloatingWindow.class));
+                if(!Settings.canDrawOverlays(MainActivity.this)){
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:"+getPackageName()));
+                    startActivityForResult(intent,REQUEST_CODE);
+                }
+                else {
+                    startService(new Intent(MainActivity.this,FloatingWindow.class));
+                }
             }
         });
+    }
+
+    @Override
+    protected void  onActivityResult(int requestcode, int resultcode, Intent data){
+        if (requestcode==REQUEST_CODE)
+        {
+            if(Settings.canDrawOverlays(this)){
+                startService(new Intent(MainActivity.this,FloatingWindow.class));
+            }
+        }
     }
 }
