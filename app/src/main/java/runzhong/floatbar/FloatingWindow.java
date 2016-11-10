6d3ca34e63dc7a1d9f8329170dc8125c66ccecc0
wrz1994 <wrz1994@gmail.com>
2016-11-10 13:59:30 -0500

    Update UI

--- app/src/main/java/runzhong/floatbar/FloatingWindow.java
+++ app/src/main/java/runzhong/floatbar/FloatingWindow.java
@@ -4,17 +4,20 @@ import android.app.Service;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.media.Image;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.DragEvent;
+import android.view.GestureDetector;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -38,9 +41,7 @@ public class FloatingWindow extends Service{
     private RecyclerView rv;
     private RecyclerView.Adapter madapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private GridLayout gl;
-    private ImageButton ib;
-    private TextView tv;
+    private FloatingActionButton fab;
     private static final String TAG = "ClipboardManager";
     private ClipboardManager mClipboardManager;
     private ClipboardManager.OnPrimaryClipChangedListener mOnPrimaryClipChangedListener=new ClipboardManager.OnPrimaryClipChangedListener() {
@@ -52,16 +53,36 @@ public class FloatingWindow extends Service{
             mLayoutManager.scrollToPosition(0);
             Log.d(TAG,newClip.toString());
 
-
-            DisplayMetrics metrics = new DisplayMetrics();
-            wm.getDefaultDisplay().getMetrics(metrics);
-            if(metrics.heightPixels>400)
-            {
-                parameters.height=400;
-                wm.updateViewLayout(ll,parameters);
-            }
         }
     };
+    /**
+     * This method converts dp unit to equivalent pixels, depending on device density.
+     *
+     * @param dp A value in dp (density independent pixels) unit. Which we need to convert into pixels
+     * @param context Context to get resources and device specific display metrics
+     * @return A float value to represent px equivalent to dp depending on device density
+     */
+    public static float convertDpToPixel(float dp, WindowManager windowManager){
+        DisplayMetrics metrics=new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(metrics);
+        float px = dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return px;
+    }
+
+    /**
+     * This method converts device specific pixels to density independent pixels.
+     *
+     * @param px A value in px (pixels) unit. Which we need to convert into db
+     * @param context Context to get resources and device specific display metrics
+     * @return A float value to represent dp equivalent to px value
+     */
+    public static float convertPixelsToDp(float px, WindowManager windowManager){
+        DisplayMetrics metrics=new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(metrics);
+        float dp = px / ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return dp;
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -73,37 +94,15 @@ public class FloatingWindow extends Service{
 
         wm =(WindowManager)getSystemService(WINDOW_SERVICE);
         ll = new LinearLayout(this);
-        ib = new ImageButton(this);
-        ib.setBackgroundColor(Color.argb(0,255,255,255));
 
 
-        ib.setImageResource(R.drawable.ic_list);
-        ib.setLayoutParams(new ViewGroup.LayoutParams(80,80));
-        ib.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        gl = new GridLayout(this);
-        gl.setColumnCount(2);
-        gl.setRowCount(1);
-        gl.setBackgroundColor(Color.argb(60,255,255,255));
-
-
-        tv = new TextView(this);
-        tv.setText("Clipboard");
-        tv.setGravity(Gravity.CENTER);
-        tv.setTextColor(Color.argb(100,0,0,0));
-        tv.setHeight(80);
-        tv.setWidth(160);
-        ib.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (rv.getVisibility() == View.VISIBLE) {
-                    rv.setVisibility(View.GONE);
-                } else {
-                    rv.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-        gl.addView(ib);
-        gl.addView(tv);
+        fab=new FloatingActionButton(new android.view.ContextThemeWrapper(this, R.style.AppTheme));
+        fab.setLayoutParams(new ViewGroup.LayoutParams((int)convertDpToPixel(40,wm),(int)convertDpToPixel(40,wm)));
+        fab.setUseCompatPadding(true);
+        fab.setClickable(true);
+        fab.setImageResource(R.drawable.ic_list);
+        fab.setCompatElevation(convertDpToPixel(6,wm));
+        fab.setSize(fab.SIZE_MINI);
 
 
         rv = new RecyclerView(this);
@@ -112,6 +111,7 @@ public class FloatingWindow extends Service{
         rv.setLayoutParams(rvParameters);
         mLayoutManager=new LinearLayoutManager(this);
         rv.setLayoutManager(mLayoutManager);
+        rv.setBackgroundColor(Color.argb(20,255,255,255));
         madapter=new mAdapter();
         rv.setAdapter(madapter);
 
@@ -121,28 +121,27 @@ public class FloatingWindow extends Service{
         ll.setGravity(Gravity.TOP);
         ll.setOrientation(ll.VERTICAL);
 
-        parameters = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.TYPE_PHONE,WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSPARENT);
+        parameters = new WindowManager.LayoutParams((int)convertDpToPixel(150,wm),(int)convertDpToPixel(300,wm),WindowManager.LayoutParams.TYPE_PHONE,WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSPARENT);
         Point size = new Point();
         wm.getDefaultDisplay().getSize(size);
-        parameters.x=size.x/2;
+        parameters.x=size.x-(int)convertDpToPixel(50,wm);
         parameters.y=size.y/3;
-        parameters.gravity= Gravity.TOP;
+        parameters.gravity= Gravity.TOP|Gravity.LEFT;
 
-        ll.addView(gl);
+        ll.addView(fab);
         ll.addView(rv);
         wm.addView(ll,parameters);
-        ll.setOnTouchListener(new View.OnTouchListener(){
+
+        fab.setOnTouchListener(new View.OnTouchListener() {
             private WindowManager.LayoutParams updatedParameters = parameters;
             int x,y;
             float touchedX, touchedY;
-
             @Override
-            public boolean onTouch(View arg0, MotionEvent event){
-                switch (event.getAction()){
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         x = updatedParameters.x;
                         y = updatedParameters.y;
-
                         touchedX=event.getRawX();
                         touchedY=event.getRawY();
                         break;
@@ -158,8 +157,22 @@ public class FloatingWindow extends Service{
                 return false;
             }
         });
-
-
+        fab.setOnClickListener(new View.OnClickListener() {
+            private WindowManager.LayoutParams updatedParameters = parameters;
+            @Override
+            public void onClick(View v) {
+                if (rv.getVisibility() == View.VISIBLE) {
+                    rv.setVisibility(View.GONE);
+                    updatedParameters.width=(int)convertDpToPixel(80,wm);
+                    updatedParameters.height=(int)convertDpToPixel(80,wm);
+                } else {
+                    updatedParameters.width=(int)convertDpToPixel(150,wm);
+                    updatedParameters.height=(int)convertDpToPixel(300,wm);
+                    rv.setVisibility(View.VISIBLE);
+                }
+                wm.updateViewLayout(ll,updatedParameters);
+            }
+        });
         mClipboardManager=(ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
         mClipboardManager.addPrimaryClipChangedListener(mOnPrimaryClipChangedListener);
 
